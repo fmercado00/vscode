841e23dbfe6ef14db8af29afefa0b15b8bcc30d5
itzeder <itzeder.bet@gmail.com>
2023-08-15 11:51:26 -0600

    new changes

--- itzeder.py
+++ itzeder.py
@@ -2,6 +2,13 @@ var = "Hello World"
 print(var)
 
 
+#example
+
+#example22
+
+git config --global user.email "itzeder.bet@gmail.com"
+git config --global user.name "itzeder"
+
 
 git config --global user.email "itzeder.bet@gmail.com"
 git config --global user.name "itzeder"
